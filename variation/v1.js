47e322f32e19cafc3f36788418fd8fe65f14b0ef
santhoshkumar <santhoshkumar.viswanthan@optiphoenix.com>
2024-09-23 16:13:08 +0530

    fix: code refactor

--- variation/v1.js
+++ variation/v1.js
@@ -1,21 +1,40 @@
-window.Kameleoon.API.Core.runWhenElementPresent(
-  ".badge.badge--bottom-left",
-  (elements) => {
-    // add the identification class to body class
-    document.body.classList.add("test-batch-bottom-variations");
-    // checking the new batch in both detuch and english language
-    const regex = /\b(Neu|New)\b/i;
+function waitUntil(predicate, time = 10000) {
+  return new Promise((resolve) => {
+    let int = setInterval(() => {
+      if (predicate()) {
+        resolve(predicate());
+        clearInterval(int);
+        int = null;
+      }
+    }, 15);
+    setTimeout(() => {
+      if (int !== null) {
+        clearInterval(int);
+        console.log("condition false");
+      }
+    }, time);
+  });
+}
 
-    // looping through each span element
-    elements.forEach(spanEle => {
+waitUntil(() => window.Kameleoon).then(() => {
+  window.Kameleoon.API.Core.runWhenElementPresent(
+    ".badge.badge--bottom-left",
+    (elements) => {
+      // add the identification class to body class
+      document.body.classList.add("test-batch-bottom-variations");
+      // checking the new batch in both detuch and english language
+      const regex = /\b(Neu|New)\b/i;
+
+      // looping through each span element
+      elements.forEach((spanEle) => {
         // checking text with regex
         if (regex.test(spanEle.innerText)) {
-            // adding class to parent element, for moving to the bottom
-            spanEle.parentElement.classList.add('bottom-batch')
+          // adding class to parent element, for moving to the bottom
+          spanEle.parentElement.classList.add("bottom-batch");
         }
-
-    });
-  },
-  null,
-  true
-);
+      });
+    },
+    null,
+    true
+  );
+});
