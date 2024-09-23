1d0faf868a0a405ebdce834f23c9503de50f33aa
santhoshkumar <santhoshkumar.viswanthan@optiphoenix.com>
2024-09-23 15:58:29 +0530

    feat: initial commit

---
+++ variation/v1.js
@@ -0,0 +1,21 @@
+window.Kameleoon.API.Core.runWhenElementPresent(
+  ".badge.badge--bottom-left",
+  (elements) => {
+    // add the identification class to body class
+    document.body.classList.add("test-batch-bottom-variations");
+    // checking the new batch in both detuch and english language
+    const regex = /\b(Neu|New)\b/i;
+
+    // looping through each span element
+    elements.forEach(spanEle => {
+        // checking text with regex
+        if (regex.test(spanEle.innerText)) {
+            // adding class to parent element, for moving to the bottom
+            spanEle.parentElement.classList.add('bottom-batch')
+        }
+
+    });
+  },
+  null,
+  true
+);
