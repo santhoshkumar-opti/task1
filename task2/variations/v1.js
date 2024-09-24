2b3813b119928a67c42e9db380185b6597c0acf8
santhoshkumar <santhoshkumar.viswanthan@optiphoenix.com>
2024-09-24 17:55:42 +0530

    fix: code refactored

--- task2/variations/v1.js
+++ task2/variations/v1.js
@@ -2,7 +2,7 @@ setTimeout(() => {
   // attach the event listener to the products container
   window.Kameleoon.API.Core.runWhenElementPresent(
     '[data-testid="plan-selection-radio-group"]',
-    ([element]) => {
+    () => {
       console.log("kameleoon added");
       document.body.classList.add("kam-Reihenfolge-Tarifvergleichsseite");
     }
