2d656fa984a2bb4ee35aca7fe5e0d36450b4f20f
santhoshkumar <santhoshkumar.viswanthan@optiphoenix.com>
2024-10-09 16:22:51 +0530

    fix: css

--- task7/script.js
+++ task7/script.js
@@ -5,21 +5,28 @@
 			Utils,
 		} = Kameleoon.API;
 
-		function changeContent([elContainer]) {
-			console.log('asdfasdfasdf');
-			console.log(elContainer);
-
+		function changeGridContent([elContainer]) {
 			elContainer.querySelector('a').href =
 				'https://www.espacemembre.macsf.fr/simulateur-tarif/habitation';
-			elContainer.querySelector('h3').innerText =
-				'Habitation';
+			elContainer.querySelector('h3').innerText = 'Habitation';
 			elContainer.querySelector('img').src =
 				'https://storage.kameleoon.com/MACSF/HPBlocDevis/habitation-loisirs.svg';
 			elContainer.querySelector('img').width = '40';
 			elContainer.querySelector('img').height = '40';
 		}
 
-		runWhenElementPresent('#rub-41723', changeContent);
+		function changeAccordianElement([aElement]) {
+
+			aElement.href = "https://www.espacemembre.macsf.fr/simulateur-tarif/auto";
+			aElement.replaceChildren('Assurance auto', aElement.children[0])
+			
+		}
+
+		runWhenElementPresent('#rub-41723', changeGridContent);
+		runWhenElementPresent(
+			'#block-contrat #flush-collapseOne .col:first-child .block-contrat-liens:first-child a:nth-child(2)',
+			changeAccordianElement
+		);
 	}
 
 	setTimeout(variation, 5000);
